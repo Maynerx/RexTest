93f22c18f909ed5d7b600a7ce104af06a0967d6e
Mayner <112780720+Maynerx@users.noreply.github.com>
2025-06-28 02:42:29 +0200

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -114,7 +114,7 @@ class Decoder(nn.Module):
     def forward(self, x: torch.Tensor, latent: torch.Tensor) -> torch.Tensor:
         x = self.embedding(x)
         for layer in self.layers:
-            x = checkpoint(layer, x, latent)
+            x = checkpoint(layer, x, latent, use_reentrant=False)
         x = self.norm(x)
         x = self.out(x)
         return x
