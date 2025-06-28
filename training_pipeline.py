49992aa2b46ba50c9d954c7051c3b515bf1ac8bf
Mayner <112780720+Maynerx@users.noreply.github.com>
2025-06-28 03:31:26 +0200

    Update training_pipeline.py

--- training_pipeline.py
+++ training_pipeline.py
@@ -118,7 +118,7 @@ class Trainer:
                 loss_ce = self.criterion(student_logits.view(-1, student_logits.size(-1)), labels.view(-1))
                 log_ps = torch.log_softmax(student_logits / self.temperature, dim=-1)
                 loss_kl = self.kl_divergence(log_ps, teacher_probs.to(DEVICE1)) * self.temperature**2
-                assert not math.isnan(ce_val), f"CE loss is NaN: {ce_val}"
+                assert not math.isnan(loss_ce), f"CE loss is NaN: {ce_val}"
                 assert not math.isnan(kl_val), f"KL loss is NaN: {kl_val}"
                 loss = self.alpha * loss_ce + self.beta * loss_kl
             self.scaler.scale(loss).backward()
