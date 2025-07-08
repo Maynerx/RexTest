9e9328a2b9199c499377d90b5cd48ecfead665a6
Mayner <112780720+Maynerx@users.noreply.github.com>
2025-07-08 22:03:32 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from xformers.ops import memory_efficient_attention
 from xformers.ops.fmha.attn_bias import LowerTriangularMask
 from xformers.ops.fmha.cutlass import FwOp as CutlassFwOp, BwOp as CutlassBwOp
 from torch._dynamo import disable
+from torch.nn.attention import SDPBackend
 
 @disable
 def scaled_dot_product_attention_grouped(
@@ -102,6 +103,54 @@ def scaled_dot_product_attention_grouped_flash(
     out = out.permute(0, 2, 1, 3)
 
     return out
+
+
+def scaled_dot_product_attention_grouped_flash_fix(
+        queries: torch.Tensor,
+        keys: torch.Tensor, 
+        values: torch.Tensor, 
+        scale: float, 
+        is_causal: bool = False,
+        dropout_p: float = 0.0
+        ) -> torch.Tensor:
+    """
+    Compute scaled dot-product attention with grouped queries.
+    
+    Args:
+        queries (torch.Tensor): Query tensor of shape (B, T_q, C).
+        keys (torch.Tensor): Key tensor of shape (B, T_k, C).
+        values (torch.Tensor): Value tensor of shape (B, T_v, C).
+        scale (float): Scaling factor for the dot product.
+        
+    Returns:
+        torch.Tensor: Output tensor after applying attention.
+    """
+    q = queries.permute(0, 2, 1, 3)
+    k = keys.permute(0, 2, 1, 3)
+    v = values.permute(0, 2, 1, 3)
+
+    bq, hq, nq, dq = q.shape
+    bk, hk, nk, dk = k.shape
+    bv, hv, nv, dv = v.shape
+
+    repeat = hq // hk
+    k = k.repeat_interleave(repeat, dim=1)  # (B, hq, Tk, d)
+    v = v.repeat_interleave(repeat, dim=1)  # (B, hq, Tv, d)
+
+    with torch.nn.attention.sdpa_kernel(SDPBackend.EFFICIENT_ATTENTION):
+        out = F.scaled_dot_product_attention(
+            query=q,
+            key=k,
+            value=v,
+            attn_mask=None,
+            dropout_p=dropout_p,
+            is_causal=is_causal,
+            scale=scale
+        )
+    out = out.permute(0, 2, 1, 3)
+
+    return out
+
 
 
 
