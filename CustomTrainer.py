641f588bf876c08ed9f23998b70c4d2bf6e76921
Mayner <112780720+Maynerx@users.noreply.github.com>
2025-08-08 04:04:36 +0200

    Update CustomTrainer.py

--- CustomTrainer.py
+++ CustomTrainer.py
@@ -67,7 +67,7 @@ class REXTrainer(Trainer):
             num_train_epochs=num_train_epochs,
             torch_compile=True,                      # Enable compilation
             torch_compile_backend="inductor",        # Choose backend
-            torch_compile_mode="max-autotune",    # Choose compile mode
+            torch_compile_mode="reduce-overhead",    # Choose compile mode
             learning_rate=lr,
             weight_decay=weight_decay,
             fp16=True,
